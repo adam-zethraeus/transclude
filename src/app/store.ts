2af11e10653cb26af0565c3cf7a63b19ca153d2a
Adam Zethraeus <adam@zethrae.us>
2020-08-03 23:55:41 -0700

    remove Coutner

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, getDefaultMiddleware, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer, { CounterState} from '../features/Counter/counterSlice';
 import pagesReducer, { Pages } from '../entities/Page/pagesSlice';
 import blocksReducer, { Blocks } from '../entities/Block/blocksSlice';
 
 
 
 export type RootState = {
-    counter: CounterState,
     blocks: Blocks
     pages: Pages
     aliases: {}
@@ -46,7 +44,6 @@ const middleware = [...getDefaultMiddleware()];
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         pages: pagesReducer,
         blocks: blocksReducer,
     },
