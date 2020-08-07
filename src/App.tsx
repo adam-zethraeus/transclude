191166fa75ca7e458966565102c756f4d8d764ce
Adam Zethraeus <adam@zethrae.us>
2020-08-07 16:50:45 -0700

    basic bootstrap layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,45 @@ import { ConnectedRouter } from 'connected-react-router'
 import { history } from './app/store';
 import Serialize from './entities/Serialize';
 import PageList from './entities/PageList';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
     return (
-        <ConnectedRouter history={history}>
-            <Switch>
-                <Redirect exact path='/' to='/list' />
-                <Route path='/page/:pageId/:drillDownBlockId?'>
-                    <PageDisplay />
-                </Route>
-                <Route path='/serialize'>
-                    <Serialize />
-                </Route>
-                <Route path='/list'>
-                    <PageList />
-                </Route>
-                <Redirect to='/' />
-            </Switch>
-        </ConnectedRouter>
+        <Container>
+            <Row>
+            </Row>
+            <Row xs={12}>
+                <Col xs={3}>
+                    <img src="/T.png" alt="" width="100%"/>
+                </Col>
+                <Col>
+                </Col>
+                <Col xs="auto" className="align-self-end">
+                    hi
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <ConnectedRouter history={history}>
+                        <Switch>
+                            <Redirect exact path='/' to='/list' />
+                            <Route path='/page/:pageId/:drillDownBlockId?'>
+                                <PageDisplay />
+                            </Route>
+                            <Route path='/serialize'>
+                                <Serialize />
+                            </Route>
+                            <Route path='/list'>
+                                <PageList />
+                            </Route>
+                            <Redirect to='/' />
+                        </Switch>
+                    </ConnectedRouter>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
