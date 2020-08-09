6395dc8ec4889395cfcf0f131fbc92f249ac7ad4
Adam Zethraeus <adam@zethrae.us>
2020-08-08 23:56:13 -0700

    breadcrumb for selected blocks

--- src/entities/Page/PageComponent.tsx
+++ src/entities/Page/PageComponent.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BlockId } from '../Block/blocksSlice';
 import Block from '../Block';
 import { PageId } from './pagesSlice';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { Link } from 'react-router-dom';
 
 export type PageComponentProps = {
     id: PageId;
@@ -14,7 +18,24 @@ export const PageComponent: React.FC<PageComponentProps> = (props) => {
     return (
         <>
             <header>
-                <h1>{props.title}</h1>
+                <Row>
+                    <Col>
+                        <h1>{props.title}</h1>
+                    </Col>
+                    { !!props.blockPath && (
+                        <>
+                            <Col>
+                            </Col>
+                            <Col xs="auto" className="align-self-center">
+                            <Breadcrumb>
+                                <Breadcrumb.Item key={props.id} linkAs={Link} linkProps={{to: `/page/${props.id}`}}>{props.title}</Breadcrumb.Item>
+                                { props.blockPath.slice(0, -1).map((id)=> <Breadcrumb.Item key={id} linkAs={Link} linkProps={{to: `/page/${props.id}/${id}`}}>{id}</Breadcrumb.Item>) }
+                                { props.blockPath.slice(-1).map((id)=> <Breadcrumb.Item key={id} active>{id}</Breadcrumb.Item>) }
+                            </Breadcrumb>
+                            </Col>
+                        </>
+                    )}
+                </Row>
             </header>
             <main>
                 { props.blockIds.map(id => <Block id={id} pageId={props.id} key={id} path={[]} />) }
