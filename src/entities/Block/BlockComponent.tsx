5ad9b24aae8cd6eec22c5fb620a96ce8897bd834
Adam Zethraeus <adam@zethrae.us>
2020-08-17 20:47:20 -0700

    factor markdown into dedicated component

--- src/entities/Block/BlockComponent.tsx
+++ src/entities/Block/BlockComponent.tsx
@@ -4,20 +4,7 @@ import { PageId } from '../Page/pagesSlice';
 import Block from './';
 import CircularReferenceBlockIndicator from '../../ui/CircularReferenceBlockIndicator';
 import Form from 'react-bootstrap/Form';
-import remarkiframe from 'remark-iframes';
-import { IFrameOpts } from '../../markdown/iframe';
-import remark2rehype from 'remark-rehype';
-import rehype2react from 'rehype-react';
-import unified from 'unified';
-import markdown from 'remark-parse';
-import sanitize from 'rehype-sanitize';
-
-var processor = unified()
-  .use(markdown)
-  .use(remark2rehype)
-  .use(remarkiframe, IFrameOpts)
-  .use(sanitize)
-  .use(rehype2react, {createElement: React.createElement})
+import Markdown from '../../markdown';
 
 export type BlockDispatchProps = {
     setSelected: () => void;
@@ -41,7 +28,7 @@ export const BlockComponent: React.FC<BlockComponentProps> = (props) => {
         <>
             { !props.isCycleRepresentation &&
                 <div className="block" onClick={(event) => { props.setSelected(); event.stopPropagation();}}>
-                    { !props.isSelected && <div className='block'>{ processor.processSync(props.content).result as React.ReactElement }</div> }
+                    { !props.isSelected && <Markdown>{ props.content } </Markdown> }
                     { props.isSelected && <Form.Control as='textarea' rows={(props.content.match(/\n/g)?.length ?? 0) + 1} value={props.content} onChange={ (event) => { props.update(event.target.value) }} /> }
                     { props.subBlockIds && props.subBlockIds.map((id) => <Block id={id} pageId={props.pageId} key={id} path={props.path.concat(props.id)} />) }
                 </div>
---
+++ src/markdown/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import remark2rehype from 'remark-rehype';
+import rehype2react from 'rehype-react';
+import unified from 'unified';
+import markdown from 'remark-parse';
+import sanitize from 'rehype-sanitize';
+import remarkiframe from 'remark-iframes';
+import { IFrameOpts } from './iframe';
+
+const processor = unified()
+  .use(markdown)
+  .use(remark2rehype)
+  .use(remarkiframe, IFrameOpts)
+  .use(sanitize)
+  .use(rehype2react, {createElement: React.createElement})
+
+type MarkdownProps = {
+	children: string[];
+};
+
+const Markdown: React.FC<MarkdownProps> = (props) => {
+    return (
+        <div className="markdown">
+            { processor.processSync(props.children.join("")).result as React.ReactElement }
+        </div>
+    );
+};
+
+export default Markdown;
